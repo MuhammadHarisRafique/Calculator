e779b2ced1beda3e33248903608d0369e40cef60
Mac <Mac@Macs-Mac.local>
2016-03-08 06:39:39 -0800

    adding in git

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var textbox: UITextField!
+       @IBOutlet weak var buttonDivide: UIButton!
+    @IBOutlet weak var buttonMultiply: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonEqual: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func btn0action(sender: AnyObject) {
+        self.textbox.text = self.textbox.text! + "0"
+    }
+    @IBAction func btn1Action(sender: AnyObject) {
+        self.textbox.text = self.textbox.text! + "1"
+    }
 
-
+    @IBAction func btn2Action(sender: AnyObject) {
+         self.textbox.text = self.textbox.text! + "2"
+    }
+    @IBAction func btn3Action(sender: AnyObject) {
+         self.textbox.text = self.textbox.text! + "3"
+    }
+    
+    @IBAction func btn4Action(sender: AnyObject) {
+         self.textbox.text = self.textbox.text! + "4"
+    }
+    
+    
+    @IBAction func btn5Action(sender: AnyObject)
+    {
+        self.textbox.text = self.textbox.text! + "5"
+    }
 }
 
